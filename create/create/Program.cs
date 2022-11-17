517684e0fb0d00d63df3296646511a03b31d289f
Pavani131 <115334212+Pavani131@users.noreply.github.com>
2022-11-17 12:36:35 +0000

    Add files via upload

---
+++ create/create/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace create
+{
+    internal class Program
+    {
+       
+        
+        static void Main(string[] args)
+        {
+            SqlConnection con = new SqlConnection("data source =.; database = student;integrated security = True");
+
+            try
+            {
+                SqlCommand cm = new SqlCommand("create table student(id int Not Null,name varchar(100),email varchar(50),join_date date)", con);
+                con.Open();
+                    cm.ExecuteNonQuery();
+                Console.WriteLine("Executed Successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oops something went wrong" +e);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            
+
+            
+        }
+    }
+}
