57b9d66c9a07234defeca123687c357187964545
Pavani131 <115334212+Pavani131@users.noreply.github.com>
2022-11-17 12:39:11 +0000

    Add files via upload

---
+++ retrive/retrive/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace retrive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = new SqlConnection("data source =.;database = student; integrated security = true");
+            con.Open();
+            try
+            {
+                SqlCommand cm = new SqlCommand("select * from student",con);
+                SqlDataReader dr = cm.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    Console.WriteLine(dr["id"] + "" + dr["name"] + "" + dr["email"]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("oops something went wrong" + e);
+            }
+            finally
+            {
+                con.Close();
+            }
+            Console.ReadLine();
+            
+            
+
+            
+            
+        }
+    }
+}
